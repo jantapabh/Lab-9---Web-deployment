697f92e73fa8ce2cec7c33d413413c2bab303539
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-15 17:34:07 +0700

    ADD COMMIT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let app = express()
 app.get('/', (req, res) => {
 
     console.log('Hello Worls')
-    res.send('Hello World')
+    res.send('Hello JanTapa 034')
 })
 
 app.get('/crash', (req, res) => {
